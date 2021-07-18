248014897de08f36c9a703e31c95f949df1f9f4a
Rohit Patro <rohitpatro6@gmail.com>
2021-07-19 00:03:49 +0530

    Added: Day6 Java Project Added

---
+++ Reverse Number/src/reversenumber/ReverseNumber.java
@@ -0,0 +1,33 @@
+package reversenumber;
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		 System.out.println("Enter the number: ");
+	        Scanner sc = new Scanner(System.in);
+
+	        int number = sc.nextInt();
+	        int result = reverseNumber(number);
+	        System.out.println(result);
+
+	    }
+
+	    //returns the number in reverse order
+	    public static int reverseNumber(int number) {
+
+	        int reverse = 0;
+
+	        while (number != 0) {
+
+	            int remainder = number % 10;
+	            reverse = reverse * 10 + remainder;
+	            number /= 10;
+
+	        }
+
+	        return reverse;
+	    }
+
+	}
+
